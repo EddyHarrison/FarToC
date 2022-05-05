9a4bf42e5a6e7d0726b164a157b9665c4d0bd874
Kalinevich <52758256+EddyHarrison@users.noreply.github.com>
2022-05-05 19:23:21 +0300

    Add files via upload

---
+++ Far to C Version2.cpp
@@ -0,0 +1,23 @@
+﻿#include<iostream>
+
+using std::cout;
+using std::cin;
+using std::endl;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    cout << "\tПеревод температуры из Фаренгейта  в Цельсия\n\n";
+
+    double Fahr;
+
+
+    for (Fahr = 0; Fahr < 101; Fahr += 0.5)
+    {
+        cout << Fahr << " = " << (Fahr - 32) / 1.8 << endl;
+    }
+
+
+    system("pause");
+    return 0;
+}
